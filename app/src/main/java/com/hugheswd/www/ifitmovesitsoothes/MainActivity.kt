caf6bb07e4f10be6226550b78d7331b118a2a8b8
Andrew Hughes <social@hugheswd.com>
2017-07-28 12:53:34 -0700

    Initial commit

---
+++ app/src/main/java/com/hugheswd/www/ifitmovesitsoothes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hugheswd.www.ifitmovesitsoothes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
